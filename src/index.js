f3ff9848ce7ae57491e5265d9cbe66c6c7a37a64
Valeria Canizales <vcanizales93@gmail.com>
2023-05-02 14:46:37 -0500

    Extracting components and module systems overview

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // 1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './App';
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -10,11 +10,7 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
-function App() {
-    return (
-        <style={{backgroundColor: "gray"}} />
-    );
-}
+
 
 // 5) Show the component on the screen
 root.render(<App />);
